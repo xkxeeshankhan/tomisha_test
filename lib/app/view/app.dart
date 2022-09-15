bd01396bbdaefd984d4666d3285a58bf3c5491c7
Xeeshan Khan <xk.xeeshankhan@gmail.com>
2022-09-15 14:17:43 +0500

    feat: start

---
+++ lib/app/view/app.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:flutter/material.dart';
+import 'package:test_app/counter/counter.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+        colorScheme: ColorScheme.fromSwatch(
+          accentColor: const Color(0xFF13B9FF),
+        ),
+      ),
+     
+      home: const CounterPage(),
+    );
+  }
+}
